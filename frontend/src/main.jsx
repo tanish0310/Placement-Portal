e2d5cb320b4b4afc17542765f5992c679515a508
tanish0310 <tanish.chowdhury0310@gmail.com>
2025-10-24 02:23:46 +0530

    added debug log

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; 
 //import "./App.css"; 
-
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
