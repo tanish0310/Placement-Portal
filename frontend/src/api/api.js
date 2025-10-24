57eeb559666517c0a2674f60abb5abcc100d4b0e
tanish0310 <tanish.chowdhury0310@gmail.com>
2025-10-24 11:49:37 +0530

    updated app.js api_base_url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
