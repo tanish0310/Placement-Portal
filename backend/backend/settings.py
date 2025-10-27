313e24ec49e7f21013d96fe9ad6314c322fa58b1
tanish0310 <tanish.chowdhury0310@gmail.com>
2025-10-28 02:30:03 +0530

    added prefix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -107,6 +107,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'PREFIX': '',
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
